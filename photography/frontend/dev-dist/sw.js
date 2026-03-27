298c880baf04242d8f4c6e4cae8c1226eb66e035
Nabil firdiansyah <nabilfirdiansyah10@gmail.com>
2026-03-27 22:45:43 +0700

    fix 2

--- photography/frontend/dev-dist/sw.js
+++ photography/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-af03485a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h2p8gm0bmb8"
+    "revision": "0.gjt0k87v5ac"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
